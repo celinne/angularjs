a877e801a999289d05f73b0f3dbc3249146e8ad0
celinne <jacquematte@gmail.com>
2015-02-28 22:59:47 -0300

    Se suben todos los archivos (js/img/fonts)

---
+++ js/controladorTareas.js
@@ -0,0 +1,7 @@
+function controladorTareas($scope){
+	$scope.tareas [
+		{texto:'Limpieza diaria', hecho:true},
+		{texto:'Ejercicios', hecho:true},
+		{texto:'Dejar de fumar', hecho: false}
+	];
+}
